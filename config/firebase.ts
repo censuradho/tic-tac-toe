5f8ec82e7544f56ef946530d552c5cd0eebabcb3
Censuradho <gustavoleiteoliveira800@gmail.com>
2022-06-06 21:02:11 -0300

    refac: firebase config to .env file

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBGh6_YsI1OTGJQd859sQKXJ1DBi7kg9eY",
-  authDomain: "tic-tac-to-e88ed.firebaseapp.com",
-  projectId: "tic-tac-to-e88ed",
-  storageBucket: "tic-tac-to-e88ed.appspot.com",
-  messagingSenderId: "1011252703721",
-  appId: "1:1011252703721:web:d1a8855048b083a31a1b78",
-  measurementId: "G-C1DRBH466C"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 } as const
